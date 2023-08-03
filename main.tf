b54c61ef2238af70b941a184c2cb68d952a8833b
Nick Wales <588472+nickwales@users.noreply.github.com>
2023-08-03 11:13:25 -0500

    Testing the acrylic service for local resolution

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "windows-server" {
   vpc_security_group_ids = [aws_security_group.aws-windows-sg.id]
   source_dest_check = false
   key_name = "nick"
-  user_data = templatefile("${path.module}/templates/user-data.tftpl", {
+  user_data = templatefile("${path.module}/templates/acrylic-dns.ps1.tftpl", {
     windows_instance_name = "windows_consul_server",
     envoy_folder    = "envoy",
     consul_folder   = "consul",
@@ -39,42 +39,42 @@ resource "aws_instance" "windows-server" {
   }
 }
 
-resource "aws_instance" "windows-server-client" {
-  ami = data.aws_ami.windows-2019.id
-  instance_type = var.windows_instance_type
-  subnet_id = var.subnet
-  vpc_security_group_ids = [aws_security_group.aws-windows-sg.id]
-  source_dest_check = false
-  key_name = "nick"
-  user_data = templatefile("${path.module}/templates/user-data.tftpl", {
-    windows_instance_name = "windows_consul_client",
-    envoy_folder    = "envoy",
-    consul_folder   = "consul",
-    consul_config_folder = "config",
-    consul_certs_folder = "config",
-    hashicups_folder    = "hashicups",
-    consul_url = var.consul_url,
-    envoy_url = var.envoy_url  
-  })
-  associate_public_ip_address = var.windows_associate_public_ip_address
+# resource "aws_instance" "windows-server-client" {
+#   ami = data.aws_ami.windows-2019.id
+#   instance_type = var.windows_instance_type
+#   subnet_id = var.subnet
+#   vpc_security_group_ids = [aws_security_group.aws-windows-sg.id]
+#   source_dest_check = false
+#   key_name = "nick"
+#   user_data = templatefile("${path.module}/templates/user-data.tftpl", {
+#     windows_instance_name = "windows_consul_client",
+#     envoy_folder    = "envoy",
+#     consul_folder   = "consul",
+#     consul_config_folder = "config",
+#     consul_certs_folder = "config",
+#     hashicups_folder    = "hashicups",
+#     consul_url = var.consul_url,
+#     envoy_url = var.envoy_url  
+#   })
+#   associate_public_ip_address = var.windows_associate_public_ip_address
   
-  # root disk
-  root_block_device {
-    volume_size           = var.windows_root_volume_size
-    volume_type           = var.windows_root_volume_type
-    delete_on_termination = true
-    encrypted             = true
-  }
-  # extra disk
-  ebs_block_device {
-    device_name           = "/dev/xvda"
-    volume_size           = var.windows_data_volume_size
-    volume_type           = var.windows_data_volume_type
-    encrypted             = true
-    delete_on_termination = true
-  }
+#   # root disk
+#   root_block_device {
+#     volume_size           = var.windows_root_volume_size
+#     volume_type           = var.windows_root_volume_type
+#     delete_on_termination = true
+#     encrypted             = true
+#   }
+#   # extra disk
+#   ebs_block_device {
+#     device_name           = "/dev/xvda"
+#     volume_size           = var.windows_data_volume_size
+#     volume_type           = var.windows_data_volume_type
+#     encrypted             = true
+#     delete_on_termination = true
+#   }
   
-  tags = {
-    Name        = "windows-server-vm-client"
-  }
-}
+#   tags = {
+#     Name        = "windows-server-vm-client"
+#   }
+# }
